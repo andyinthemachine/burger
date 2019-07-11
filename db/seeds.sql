965d1801a4361b2f5584a922ec30c85ca32e0211
Andrew Ulrich <andyinthemachine@hotmail.com>
2019-07-11 08:04:39 -0600

    initial setup

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burger (name) VALUES ('Bleu Cheese');
+INSERT INTO burger (name) VALUES ('Lamb and Avocado');
+INSERT INTO burger (name, eaten) VALUES ('Paddy Smith', true);
+INSERT INTO burger (name, eaten) VALUES ('Open Sesame', true);
+INSERT INTO burger (name, eaten) VALUES ('Juicy Lucy', true);
+INSERT INTO burger (name) VALUES ('Steemed Cheeseburger');
---
+++ public/assets/js/burgers.js
@@ -0,0 +1,60 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+  $(".change-eaten").on("click", function(event) {
+    var id = $(this).data("id");
+    var newEaten = $(this).data("neweaten");
+
+    var newEatenState = {
+      eaten: newEaten
+    };
+
+    // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newEatenState
+    }).then(
+      function() {
+        console.log("changed eaten to", newEaten);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
+  $(".create-form").on("submit", function(event) {
+        // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+    var newBurger = {
+      name: $("#add-burger").val().trim(),
+      eaten: $("[name=eaten]:checked").val().trim()
+    };
+
+    // Send the POST request.
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function() {
+        console.log("created new burger");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
+  $(".delete-burger").on("click", function(event) {
+    var id = $(this).data("id");
+
+    // Send the DELETE request.
+    $.ajax("/api/burgers/" + id, {
+      type: "DELETE"
+    }).then(
+      function() {
+        console.log("deleted burger", id);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+});
